16b404914139a0d9409d1cfa75f28adc4545c538
Kyrylo <fresh.js.ftd.@gmail.com>
2025-01-16 20:49:14 +0200

    Fix migration file and add migration adjacent commands

--- migrations/20250116180110-create-user.js
+++ migrations/20250116180110-create-user.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('users', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -10,37 +10,52 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       firstName: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(128),
+        // NOT NULL 
+        allowNull: false,
+        // вказати іншу назву поля у БД
+        field: 'first_name'
       },
       lastName: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(128),
+        allowNull: false,
+        field: 'last_name'
       },
       email: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(300),
+        allowNull: false,
+        // обмеження унікальності
+        unique: true
       },
       password: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
+        allowNull: false,
       },
       isMale: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
+        field: 'is_male',
       },
       balance: {
-        type: Sequelize.DECIMAL
+        type: Sequelize.DECIMAL(9, 2),
+        // DEFAULT у SQL
+        defaultValue: 0
       },
       birthday: {
         type: Sequelize.DATEONLY
       },
       createdAt: {
         allowNull: false,
+        field: 'created_at',
         type: Sequelize.DATE
       },
       updatedAt: {
         allowNull: false,
+        field: 'updated_at',
         type: Sequelize.DATE
       }
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    await queryInterface.dropTable('users');
   }
 };
