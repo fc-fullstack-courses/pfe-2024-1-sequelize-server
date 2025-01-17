2e5c1539499cdcf78afc7f196da782c65557b0c8
Kyrylo <fresh.js.ftd.@gmail.com>
2025-01-17 19:09:35 +0200

    Add Sequelize validation examples with date-fns

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 'use strict';
 const { Model } = require('sequelize');
+const { isAfter, addYears } = require("date-fns");
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -19,20 +20,38 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING(128),
         allowNull: false,
         field: 'first_name',
+        // валідація на рівні моделі (до запиту в БД)
+        validate: {
+          notEmpty: true,
+          notNull: true,
+        }
       },
       lastName: {
         type: DataTypes.STRING(128),
         allowNull: false,
         field: 'last_name',
+        validate: {
+          notEmpty: true,
+          notNull: true,
+        }
       },
       email: {
         type: DataTypes.STRING(300),
         allowNull: false,
         unique: true,
+        validate: {
+          notEmpty: true,
+          notNull: true,
+          isEmail: true
+        }
       },
       password: {
         type: DataTypes.TEXT,
         allowNull: false,
+        validate: {
+          notEmpty: true,
+          notNull: true,
+        }
       },
       isMale: {
         type: DataTypes.BOOLEAN,
@@ -40,10 +59,23 @@ module.exports = (sequelize, DataTypes) => {
       },
       balance: {
         type: DataTypes.DECIMAL(9, 2),
-        defaultValue: 0
+        defaultValue: 0,
+        validate: {
+          isNumeric: true,
+          min: 0
+        }
       },
       birthday: {
         type: DataTypes.DATEONLY,
+        validate: {
+          isDate: true,
+          // користувацький валідатор
+          isAdultUser (userBirthday) {
+            if(isAfter(new Date(userBirthday), addYears(new Date(), -18))) {
+              throw new Error('User must be adult')
+            }
+          }
+        }
       },
     },
     //налаштування
