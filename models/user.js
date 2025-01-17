ee8fe6d5d9aa6abcbf129b9febbad2d9cb075f79
Kyrylo <fresh.js.ftd.@gmail.com>
2025-01-17 18:35:32 +0200

    Update User model to be identical to db table state

--- models/user.js
+++ models/user.js
@@ -1,7 +1,5 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
+const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -13,17 +11,51 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  User.init({
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.TEXT,
-    isMale: DataTypes.BOOLEAN,
-    balance: DataTypes.DECIMAL,
-    birthday: DataTypes.DATEONLY
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
+  User.init(
+    // схема атрибутів нашої сутності в БД
+    {
+      // описи атрибутів += однакові з описами у міграціях
+      firstName: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        field: 'first_name',
+      },
+      lastName: {
+        type: DataTypes.STRING(128),
+        allowNull: false,
+        field: 'last_name',
+      },
+      email: {
+        type: DataTypes.STRING(300),
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      isMale: {
+        type: DataTypes.BOOLEAN,
+        field: 'is_male',
+      },
+      balance: {
+        type: DataTypes.DECIMAL(9, 2),
+        defaultValue: 0
+      },
+      birthday: {
+        type: DataTypes.DATEONLY,
+      },
+    },
+    //налаштування
+    {
+      sequelize,
+      modelName: 'User',
+      tableName: 'users', // ручне встановлення назви таблиі
+      underscored: true
+      // timestamps: false - вимкнути таймштампи
+      // createdAt: 'created_at',
+      // updatedAt: 'updated_at'
+    }
+  );
   return User;
 };
