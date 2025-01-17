da0d036ce33e4817f9e99c99f80cdc2f460dfa54
Kyrylo <fresh.js.ftd.@gmail.com>
2025-01-17 21:05:48 +0200

    Add User model SELECT replacements examples

--- controllers/userController.js
+++ controllers/userController.js
@@ -26,12 +26,12 @@ module.exports.deleteUser = async (req, res, next) => {
   //     id : 2
   //   }
   // });
-  
+
   // deletedUser - кількість видаленних рядків завжди
   const deletedUser = await User.destroy({
     where: {
-      id : userId
-    }
+      id: userId,
+    },
   });
 
   res.status(200).send(`User with id ${userId} deleted`);
@@ -40,13 +40,13 @@ module.exports.deleteUser = async (req, res, next) => {
 module.exports.updateUser = async (req, res, next) => {
   const {
     params: { userId },
-    body: newUserData
+    body: newUserData,
   } = req;
 
   // UPDATE users SET ... WHERE id = 1;
   const result = await User.update(newUserData, {
     where: {
-      id: userId
+      id: userId,
     },
     // RETURNING *
     returning: true,
@@ -54,13 +54,48 @@ module.exports.updateUser = async (req, res, next) => {
     // returning: ['first_name', 'last_name']
   });
 
-  const [rowsUpdated,[updatedUser]] = result;
+  const [rowsUpdated, [updatedUser]] = result;
 
   res.status(200).send(updatedUser);
 };
 
 module.exports.getUsers = async (req, res, next) => {
-  res.status(200).send('All Users returned');
+  // SELECT * FROM users;
+  // const users = await User.findAll();
+
+  // SELECT id, email, is_male FROM users;
+  // const users = await User.findAll({
+  //   attributes: ['id', 'email', 'isMale'],
+  // });
+
+  // SELECT id, email, is_male AS gender FROM users;
+  // const users = await User.findAll({
+  //   attributes: ['id', 'email', ['is_male', 'gender']],
+  // });
+
+  // SELECT все окрім паролю AS gender FROM users;
+  // const users = await User.findAll({
+  //   attributes: {
+  //     exclude: ['password', 'email'],
+  //   },
+  // });
+
+  // SELECT *  FROM users WHERE first_name = "User";
+  // const users = await User.findAll({
+  //   where: {
+  //     firstName: 'User'
+  //   }
+  // });
+
+  // SELECT *  FROM users WHERE first_name = "User" AND lastName = 'Userenko';
+  const users = await User.findAll({
+    where: {
+      firstName: 'User',
+      lastName: 'Userenko',
+    },
+  });
+
+  res.status(200).send(users);
 };
 
 module.exports.getUser = async (req, res, next) => {
@@ -68,5 +103,23 @@ module.exports.getUser = async (req, res, next) => {
     params: { userId },
   } = req;
 
-  res.status(200).send('User returned');
+  // SELECT * FROM users WHERE id = 1;
+  // const [user] = await User.findAll({
+  //   where: {
+  //     id: userId
+  //   }
+  // });
+
+  // через первинний ключ
+  const user = await User.findByPk(userId);
+
+  // SELECT * FROM users WHERE email = 'admin@gmail.com';
+  // повернути перший підходящий запис
+  // const user = await User.findOne({
+  //   where: {
+  //     email: 'admin@gmail.com'
+  //   }
+  // });
+
+  res.status(200).send(user);
 };
