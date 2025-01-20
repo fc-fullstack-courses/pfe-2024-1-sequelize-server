9d0272c08a1586253f7c5d21dfc495d377dca734
Kyrylo <fresh.js.ftd.@gmail.com>
2025-01-20 20:47:37 +0200

    Add base Todo model

---
+++ models/todo.js
@@ -0,0 +1,44 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Todo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Todo.init(
+    {
+      body: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validate: {
+          notNull: true,
+          notEmpty: true
+        }
+      },
+      isDone: {
+        type: DataTypes.BOOLEAN,
+        field: 'is_done',
+        defaultValue: false
+      },
+      deadline: {
+        type: DataTypes.DATE,
+        validate: {
+          isDate: true
+        }
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Todo',
+      tableName: 'todos',
+      underscored: true,
+    }
+  );
+  return Todo;
+};
