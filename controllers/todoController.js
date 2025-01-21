2fab0f1b1395155585079b9ac25a32bbbe00fb8e
Kyrylo <fresh.js.ftd.@gmail.com>
2025-01-21 18:51:28 +0200

    Add rest of CRUD for Todo with some magic method usage

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,10 +1,11 @@
+const NotFoundError = require('../errors/NotFound');
 const { Todo } = require('../models');
 
 module.exports.createTodo = async (req, res, next) => {
   try {
     const {
       body,
-      user
+      user,
       // params: { userId },
     } = req;
 
@@ -13,7 +14,6 @@ module.exports.createTodo = async (req, res, next) => {
     //   userId,
     //   ...body,
     // });
-
 
     // магічний метод секвалайза
     const todo = await user.createTodo(body);
@@ -24,3 +24,102 @@ module.exports.createTodo = async (req, res, next) => {
   }
 };
 
+module.exports.getTodos = async (req, res, next) => {
+  try {
+    const {
+      user,
+      // params: { userId },
+    } = req;
+
+    // const todos = await Todo.findAll({
+    //   where: {
+    //     userId,
+    //   },
+    // });
+
+    const todos = await user.getTodos();
+
+    res.status(200).send({ data: todos });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.getTodo = async (req, res, next) => {
+  try {
+    const {
+      user,
+      params: { userId, todoId },
+    } = req;
+
+    const todo = await Todo.findOne({
+      where: {
+        id: todoId,
+        userId,
+      },
+    });
+
+    if (!todo) {
+      throw new NotFoundError('Todo not found');
+    }
+
+    res.status(200).send({ data: todo });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.updateTodo = async (req, res, next) => {
+  try {
+    const {
+      // user,
+      params: { userId, todoId },
+      body,
+    } = req;
+
+    const [todosUpdated, [todo]] = await Todo.update(body, {
+      where: {
+        id: todoId,
+        userId,
+      },
+      returning: true,
+    });
+
+    if (todosUpdated !== 1) {
+      throw new NotFoundError('Todo not found');
+    }
+
+    res.status(200).send({ data: todo });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports.deleteTodo = async (req, res, next) => {
+  try {
+    const {
+      user,
+      params: { userId, todoId },
+    } = req;
+
+    const todo = await Todo.findOne({
+      where: {
+        id: todoId,
+        userId,
+      },
+    });
+
+    if (!todo) {
+      throw new NotFoundError('Todo not found');
+    }
+
+    // не видаляє а відв'язує від юзера
+    // await user.removeTodo(todo);
+
+    await todo.destroy();
+
+    res.status(200).send({ data: todo });
+  } catch (error) {
+    next(error);
+  }
+};
--- routers/userRouter.js
+++ routers/userRouter.js
@@ -11,5 +11,10 @@ userRouter.put('/:userId', findUserById, UserController.updateUser);
 userRouter.delete('/:userId', findUserById, UserController.deleteUser);
 
 userRouter.post('/:userId/todos', findUserById, TodoController.createTodo);
+userRouter.get('/:userId/todos', findUserById, TodoController.getTodos);
+
+userRouter.get('/:userId/todos/:todoId', findUserById, TodoController.getTodo);
+userRouter.put('/:userId/todos/:todoId', findUserById, TodoController.updateTodo);
+userRouter.delete('/:userId/todos/:todoId', findUserById, TodoController.deleteTodo);
 
 module.exports = userRouter;
